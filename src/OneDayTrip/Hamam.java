7beb51d5949f986a7ef00a58ac3a7b3ac2af5e82
Mrc <kahtani@protonmail.com>
2022-05-18 16:21:20 +0300

    cleaner

--- src/OneDayTrip/Hamam.java
+++ src/OneDayTrip/Hamam.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Hamam extends Abstract_Etkinlik{
     public static void main(String[] args) {
-        welcome("\n1-Türk Hamamı - 100TL\n2-Sauna - 105TL\n3-Masaj - 150TL\n4-Cıkış", "HAMAMA HOŞGELDİNİZ" , 100, 105, 150) ;
+        welcome("\n1-Türk Hamamı - 100TL\n2-Sauna - 105TL\n3-Masaj - 150TL\n4-Cıkış",
+                "HAMAMA HOŞGELDİNİZ" , 100, 105, 150) ;
     }
 
 }
--- src/OneDayTrip/bogazTur.java
+++ src/OneDayTrip/bogazTur.java
@@ -2,7 +2,8 @@ package OneDayTrip;
 
 public class bogazTur extends Abstract_Etkinlik {
     public static void main(String[] args) {
-        welcome("\n1-Boğaz Turu - 200TL\n2-Fasıl - 125TL\n3-Yemek menü(balık) - 150TL\n4-Cıkış", "BOĞAZ TURUNA HOŞGELDİNİZ" ,200, 125, 150) ;
+        welcome("\n1-Boğaz Turu - 200TL\n2-Fasıl - 125TL\n3-Yemek menü(balık) - 150TL\n4-Cıkış",
+                "BOĞAZ TURUNA HOŞGELDİNİZ" ,200, 125, 150) ;
     }
 
 }
